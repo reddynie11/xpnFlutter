a1da398313ee6a79f44c4f7365bd44e0a66363b7
reddynie11 <reddy.nie11@gmail.com>
2020-06-24 23:17:09 +0530

    userInput

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './txn.dart';
 
@@ -9,8 +10,8 @@ class MyApp extends StatelessWidget {
     Txn(id: 't1', title: 'Groceries', amount: 1000, date: DateTime.now()),
     Txn(id: 't2', title: 'Shirts', amount: 2500, date: DateTime.now()),
     Txn(id: 't3', title: 'Shoes', amount: 4000, date: DateTime.now()),
-    Txn(id: 't4', title: 'Restaurant', amount: 1000, date: DateTime.now()),
-    Txn(id: 't5', title: 'Fuel', amount: 2000, date: DateTime.now()),
+    // Txn(id: 't4', title: 'Restaurant', amount: 1000, date: DateTime.now()),
+    // Txn(id: 't5', title: 'Fuel', amount: 2000, date: DateTime.now()),
   ];
   @override
   Widget build(BuildContext context) {
@@ -32,6 +33,46 @@ class MyApp extends StatelessWidget {
                 //elevation: 5,
               ),
             ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Description'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text('+ Add Reciept'),
+                          onPressed: () {},
+                          textColor: Colors.purple,
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: Colors.purple,
+                                  width: 2,
+                                  style: BorderStyle.solid)),
+                        ),
+                        RaisedButton(
+                          child: Text('Add Expense'),
+                          onPressed: () {},
+                          color: Colors.purple[600],
+                          textColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Column(
                 children: transactions.map((txn) {
               return Card(
@@ -49,18 +90,24 @@ class MyApp extends StatelessWidget {
                             ),
                             borderRadius: BorderRadius.circular(10)),
                         child: Text(
-                          txn.amount.toString(),
+                          'INR ${txn.amount}',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 18,
                               color: Colors.purple),
-                              
                         )),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(txn.title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                        Text(txn.date.toString(), style: TextStyle(color: Colors.grey),),
+                        Text(
+                          txn.title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          DateFormat.yMMMd().format(txn.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ],
                     ),
                   ],
